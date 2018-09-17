9800a876cec328a13ef959e421f6a0e3a8eddf19
Joe Liao <joe83830@gmail.com>
2018-09-17 13:09:00 -0400

    Final

--- HW2/hashtable_chaining.py
+++ HW2/hashtable_chaining.py
@@ -35,61 +35,71 @@ class HashTable:
   # Inserts the `(key, value)` pair into the hash table, overwriting any value
   # previously associated with `key`.
   # Note: Neither `key` nor `value` may be None (an exception will be raised)
-  def insert(self, key, value):
+  def insert(self, key=None, value=None):
 
     try:
       if key is None or value is None:
         raise TypeError
+
+      ind = cs5112_hash1(key) % self.array.size
+      head = self.array.get(ind)
+
+      while head != None:
+
+        if head.get_value()[0] == key:
+          head.set_value((key, value))
+          return
+
+        head = head.get_next()
+
+      self.item_count += 1
+      # print("SIZE after insert = " + str(self.item_count))
+
+      head = self.array.get(ind)
+      new_node = SLLNode((key,value))
+
+      new_node.set_next(head)
+      self.array.set(ind, new_node)
+
+      if (self.item_count / self.array.size) > self.load_factor:
+        temp_arr = self.array
+        self._resize_array()
+        self.item_count = 0
+
+        for i in range(temp_arr.size):
+
+          oldhead = temp_arr.get(i)
+          while oldhead != None:
+            self.insert(oldhead.get_value()[0], oldhead.get_value()[1])
+            oldhead = oldhead.get_next()
+
     except TypeError:
       print('Key and/or value cannot be None!')
-
-    ind = cs5112_hash1(key) % self.array.size
-    head = self.array.get(ind)
-
-    while head != None:
-
-      if head.get_value()[0] == key:
-        head.set_value((key, value))
-
-      head = head.get_next()
-
-    self.item_count += 1
-    head = self.array.get(ind)
-    new_node = SLLNode((key,value))
-
-    new_node.set_next(head)
-    self.array.set(ind, new_node)
-
-    if (self.item_count / self.array.size) > self.load_factor:
-      temp_arr = self.array
-      self._resize_array()
-
-      for oldhead in temp_arr:
-        while oldhead != None:
-          self.insert(oldhead.get_value()[0], oldhead.get_value()[1])
-          oldhead = oldhead.get_next()
 
   # Returns the value associated with `key` in the hash table, or None if no
   # such value is found.
   # Note: `key` may not be None (an exception will be raised)
-  def get(self, key):
+  def get(self, key=None):
 
     try:
       if key is None:
         raise TypeError
+
+      ind = cs5112_hash1(key) % self.array.size
+      head = self.array.get(ind)
+
+      while head != None:
+        if head.get_value()[0] == key:
+          return head.get_value()[1]
+
+        head = head.get_next()
+
+      return None
+
     except TypeError:
       print('Key cannot be None!')
 
-    ind = cs5112_hash1(key) % self.array.size
-    head = self.array.get(ind)
 
-    while head != None:
-      if head.get_value()[0] == key:
-        return head.get_value()[1]
-
-      head = head.get_next()
-
-    return None
 
   # Removes the `(key, value)` pair matching the given `key` from the map, if it
   # exists. If such a pair exists in the map, the return value will be the value
@@ -97,36 +107,38 @@ class HashTable:
   # Note: `key` may not be None (an exception will be raised)
   def remove(self, key):
 
-
     try:
       if key is None:
         raise TypeError
+
+      ind = cs5112_hash1(key) % self.array_size
+      head = self.array.get(ind)
+      prev = None
+
+      print("HEAD = " + str(head))
+
+      while head != None:
+        if head.get_value()[0] == key:
+          break
+
+        prev = head
+        head = head.get_next()
+
+      if not head:
+        return None
+
+      self.item_count -= 1
+
+      if prev != None:
+        prev.set_next(head.get_next())
+      else:
+        self.array.set(ind, head.get_next())
+
+      return head.get_value()[1]
+
     except TypeError:
       print('Key cannot be None!')
 
-    ind = cs5112_hash1(key) % self.array_size
-    head = self.array.get(ind)
-    prev = None
-
-    while head != None:
-
-      if head.get_value()[0] == key:
-        break
-
-      prev = head
-      head = head.get_next()
-
-    if not head:
-      return None
-
-    self.item_count -= 1
-
-    if prev != None:
-      prev.set_next(head.get_next())
-    else:
-      self.array.set(ind, head.get_next())
-
-    return head.get_value()[1]
 
   # Returns the number of elements in the hash table.
   def size(self):
@@ -135,8 +147,9 @@ class HashTable:
   # Internal helper function for resizing the hash table's array once the ratio
   # of stored mappings to array size exceeds the specified load factor.
   def _resize_array(self):
-    # YOUR CODE HERE
+
     self.array = FixedSizeArray(2 * self.array.size)
+    self.array_size *= 2
 
   # Internal helper function for accessing the array underlying the hash table.
   def _get_array(self):
